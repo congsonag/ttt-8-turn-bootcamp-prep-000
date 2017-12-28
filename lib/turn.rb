e2e383a9a990700538132d1781ee8126ac9dc490
Stephane <son.trinh.dev@gmail.com>
2017-12-28 03:40:44 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,31 +6,3 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(str)
-  str.chomp.to_i - 1
-end
-
-def position_taken?(board, position)
-  ['X', 'O'].include?(board[position])
-end
-
-def valid_move?(board, input)
-  index = gets.chomp.to_i - 1
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
-def move(board, index, token='X')
-  board[index] = token
-end
-
-def turn(board)
-  puts 'Please enter 1-9:'
-  input = gets.chomp
-  if valid_move?(board,input)
-    move(board, input_to_index(input))
-    display_board(board)
-  else
-    turn(board)
-  end
-
-end
